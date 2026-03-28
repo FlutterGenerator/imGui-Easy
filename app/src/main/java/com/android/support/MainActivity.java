ba8f4d4dcda54248900b445cc4c4ef196540ec5b
tojik_proof_93 <elementman50@yandex.com>
2026-03-28 17:25:40 +0500

    Update MainActivity.java

--- app/src/main/java/com/android/support/MainActivity.java
+++ app/src/main/java/com/android/support/MainActivity.java
@@ -1,14 +1,7 @@
 package com.android.support;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.provider.Settings;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
